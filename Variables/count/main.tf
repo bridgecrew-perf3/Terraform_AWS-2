8ce7b1e9cbe5c23a9a745b37107810f385351bc6
aruntheja21 <87597292+aruntheja21@users.noreply.github.com>
2021-09-21 14:17:16 +0530

    main.tf

---
+++ Variables/count/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "AKIATQ37NXB2AYK7R6PQ"
+   secret_key = "S1Yg1Qm2JNSej8EHdhPTiu5l5ZD36URs3ed2NyYT"
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = var.instance_count
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
+
+variable "instance_count" {
+  description = "EC2 instance count"
+  type        = number
+  default     = 2
+}
