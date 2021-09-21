ec3d97a0bf584eccc0e4f12aa09708fde3e1d53b
aruntheja21 <87597292+aruntheja21@users.noreply.github.com>
2021-09-21 13:20:52 +0530

    main.tf

---
+++ List/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "AKIATQ37NXB2OBQHAALW"
+   secret_key = "ilKygurap8zSErv7jySTDi2796WGqMkEtN6txNHf"
+}
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = 1
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["user1", "user2", "user3s"]
+}
